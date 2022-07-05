6d5bbaf883ad4d665bdbe643cd1a7f690482d91c
Karani33 <karanidenisw@gmail.com>
2022-07-05 11:14:36 +0300

    1. My first toolbox

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -2,28 +2,13 @@
 from calculator_1 import add, sub, mul, div
 
 
-def add(a, b):
+def main():
     a = 10
     b = 5
-    print("{} + {} = {}".format(a, b, add(a + b)))
-
-
-def main(a, b):
-    a = 10
-    b = 5
-    print("{} - {} = {}".format(a, b, sub(a-b)))
-
-
-def main(a, b):
-    a = 10
-    b = 5
-    print("{} * {} = {}".format(a, b, mul(a*b)))
-
-
-def main(a, b):
-    a = 10
-    b = 5
-    print("{} / {} = {}".format(a, b, div(a/b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
 
 
 if __name__ == '__main__':
