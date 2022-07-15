5e3c086122df644057c52901e4ab23eb068b52ad
Karani33 <karanidenisw@gmail.com>
2022-07-15 21:24:02 +0200

    2. Unique addition

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,18 +1,10 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     mem = []
+    for item in my_list:
+        if item not in mem:
+            mem.append(item)
     sum = 0
-    # get each item from my_list
-    # check if item is not in mem
-       # if the above is true, add item to mem
-    # otherwise do nothing
-
-
-    # sum up all the values in mem
-
-
-
-my_list = [1, 2, 3, 1, 4, 2, 5]
-result = uniq_add(my_list)
-print(result)
-
+    for item in mem:
+        sum += item
+        return sum
