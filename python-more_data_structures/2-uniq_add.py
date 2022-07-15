a1bd9724720b044a58c2170ee92996db1ab64192
Karani33 <karanidenisw@gmail.com>
2022-07-15 20:14:57 +0200

    3. Present in both

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    mem = []
+    sum = 0
+    # get each item from my_list
+    # check if item is not in mem
+       # if the above is true, add item to mem
+    # otherwise do nothing
+
+
+    # sum up all the values in mem
+
+
+
+my_list = [1, 2, 3, 1, 4, 2, 5]
+result = uniq_add(my_list)
+print(result)
+
