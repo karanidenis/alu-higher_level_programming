49bd48322c1c61af71c568d2e855f153cd781f86
Karani33 <karanidenisw@gmail.com>
2022-07-15 15:08:26 +0200

    6. Lists of lists = Matrix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -8,4 +8,4 @@ def print_matrix_integer(matrix=[[]]):
                 y.append(j)
             for i in row:
                 x.append(i)
-            print("{:d}".format(matrix[i][j]), end = "")
+            print("{:d}".format(matrix[x][y]), end = "")
