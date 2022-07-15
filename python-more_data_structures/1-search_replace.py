531e34f3ede597c31663955738318ed7230dcfe1
Karani33 <karanidenisw@gmail.com>
2022-07-15 18:00:51 +0200

    1. Search and replace

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    copy_list = my_list.copy
+    copy_list = []
     for i in range(len(my_list)):
         if my_list[i] == search:
             copy_list.append(replace)
