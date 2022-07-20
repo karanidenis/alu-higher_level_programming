7e903afc446143d8e0387d103ced4b8ebdd68ae5
Karani33 <karanidenisw@gmail.com>
2022-07-20 10:51:15 +0200

    printing elements of a list using try and except

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,10 +4,11 @@ def safe_print_list(my_list=[], x=0):
     for i in range(x):
         try:
             print(my_list[i], end='')
+            i += 1
         except IndexError:
             pass
     print()
-    return i + 1
+    return i
 
 
 if __name__ == '__main__':
