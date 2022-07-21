d82113833c2ad6e42a5c1838235c5222fe30aac5
Karani33 <karanidenisw@gmail.com>
2022-07-22 00:14:49 +0200

    defines a square by attribute size with a message

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-# defining a square by:
-# Private instance attribute: size
-# Instantiation with size (no type/value verification)
-# You are not allowed to import any module
+"""defining a square by:
+Private instance attribute: size
+ Instantiation with size (no type/value verification)
+ You are not allowed to import any module"""
+
 
 class Square:
+    """ define a square"""
     def __init__(self, size=0):
         """
         Creates an instance of Square
@@ -19,37 +21,3 @@ class Square:
             raise ValueError("size must be >= 0")
 
         self.__size = size
-
-
-if __name__ == '__main__':
-    my_square_1 = Square(3)
-    print(type(my_square_1))
-    print(my_square_1.__dict__)
-
-    my_square_2 = Square()
-    print(type(my_square_2))
-    print(my_square_2.__dict__)
-
-    try:
-        print(my_square_1.size)
-    except Exception as e:
-        print(e)
-
-    try:
-        print(my_square_1.__size)
-    except Exception as e:
-        print(e)
-
-    try:
-        my_square_3 = Square("3")
-        print(type(my_square_3))
-        print(my_square_3.__dict__)
-    except Exception as e:
-        print(e)
-
-    try:
-        my_square_4 = Square(-89)
-        print(type(my_square_4))
-        print(my_square_4.__dict__)
-    except Exception as e:
-        print(e)
