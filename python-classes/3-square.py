2e7745b413df03b6e96b96a507153a235fd5ce58
Karani33 <karanidenisw@gmail.com>
2022-07-22 00:33:05 +0200

    defines a square by attribute size with a message and a public instance area

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -3,9 +3,9 @@
 define a square by:
 Private instance attribute: size
 Instantiation with optional size: def __init__(self, size=0):
-size must be an integer, otherwise raise a TypeError exception with the message size must be an integer
-if size is less than 0, raise a ValueError exception with the message size must be >= 0
-Public instance method: def area(self): that returns the current square area
+size must be int, raise TypeError exception(message size must be an integer)
+if size is < 0, raise ValueError exception(message size must be >= 0)
+Public instance method: def area(self): returns the current square area
 You are not allowed to import any module
 """
 
