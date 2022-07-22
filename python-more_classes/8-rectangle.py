af75782cd9955c8b2eac0e131fc20091dd087418
Karani33 <karanidenisw@gmail.com>
2022-07-22 15:14:09 +0200

    static method

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -148,8 +148,3 @@ class Rectangle:
         if rect_1.area() >= rect_2.area():
             return rect_1
         return rect_2
-
-    @classmethod
-    def square(cls, size=0):
-
-       return cls(size, size)
