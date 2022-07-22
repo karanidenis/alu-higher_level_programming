e883a233c94eb32ad267c566c4754127fc4aac96
Karani33 <karanidenisw@gmail.com>
2022-07-22 11:10:52 +0200

    defines a square by attribute size with a message and a public instance area using property setter and property to retrieve and replace area with #

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -75,8 +75,6 @@ class Square:
         """
         replace area with #
         """
-        for i in range(self.__size):
-            print("#" * self.__size)
 
         if self.__size == 0:
             print()
