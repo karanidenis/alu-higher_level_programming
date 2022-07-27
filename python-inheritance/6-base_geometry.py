ca54cee07913482ec10f56c77a93c82718025f1b
Karani33 <karanidenisw@gmail.com>
2022-07-28 00:49:05 +0200

    class BaseGeometry with an exception message

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" class BaseGeometry """
+
+
+class BaseGeometry:
+    def area(self):
+        """
+        parameter self
+        raise and exception with a message
+        """
+        raise Exception("area() is not implemented")
+
+
+if __name__ == '__main__':
+    bg = BaseGeometry()
+
+    try:
+        print(bg.area())
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
