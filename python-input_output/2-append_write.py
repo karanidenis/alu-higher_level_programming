73aa1e86bc24d9417689f442336bfdf841e85495
Karani33 <karanidenisw@gmail.com>
2022-07-28 22:41:45 +0200

    function that appends a file and counts the number of characters

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -5,10 +5,15 @@
 def write_file(filename="", text=""):
 
     """
-     function creates a new file or
-    appends current one and counts number of characters added
+     function creates a new file or appends current one
+     and counts number of characters added
+     Args:
+        filename (str): The name of the file to append to.
+        text (str): The string to append to the file.
+    Returns:
+        The number of characters appended.
     """
 
     with open(filename, mode='a', encoding='utf-8') as a_text:
 
-        return a_text.write(str(text))
+        return a_text.write(text)
