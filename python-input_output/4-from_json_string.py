6b043d813b2b99ef27ea19ef0a53a001bf88252b
Karani33 <karanidenisw@gmail.com>
2022-07-28 23:19:26 +0200

    python representation from json representation

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-""" defines a function that returns python data structure from Json representation """
+""" defines a function that returns python
+data structure from Json representation """
 
 import json
 
 
 def from_json_string(my_str):
-    """ function that returns python representation"""
+    """ returns python representation"""
     return json.loads(my_str)
