d170c43c1b2e2c26049d631e4054446be4b7ef63
Karani33 <karanidenisw@gmail.com>
2022-07-28 22:18:54 +0200

    function that overwrites a file and counts the number of characters

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -5,6 +5,6 @@
 def write_file(filename="", text=""):
     """ function creates a new file or
     overwrites current one and counts number of characters """
-    with open(filename, mode='w', encoding='utf-8') as text:
-        return text.read()
-
+    with open(filename, mode='w', encoding='utf-8') as a_text:
+        text = a_text.write("")
+        return a_text.read()
