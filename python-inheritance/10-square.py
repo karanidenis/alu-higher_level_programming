cc9bdb2e1eb594b1d416f4f1a1f61300863e6f69
Karani33 <karanidenisw@gmail.com>
2022-07-28 12:05:37 +0200

    import, class inherits from another and calculates area and string representation

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -9,14 +9,10 @@ class Square(Rectangle):
 
     def __init__(self, size):
         """ initialises square """
-        self.integer_validator = ("size", size)
         self.__size = size
-
-    def area(self):
-        return self.__size**2
-
-    def __str__(self):
-        return "[Square]" + str(self.__size) + "/" + str(self.__size)
+        self.integer_validator = ("size", size)
+        super().__init__(size, size)
+        self._size = size
 
 
 if __name__ == '__main__':
