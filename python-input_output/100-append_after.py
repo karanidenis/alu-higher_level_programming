977c6dacc8703c42a5e4f6097811d1423570c09e
Karani33 <karanidenisw@gmail.com>
2022-07-29 00:55:34 +0200

    adding lines

---
+++ python-input_output/100-append_after.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" inserting lines to text file"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """
+    :param filename: file name
+    :param search_string: insert line where there's search string
+    :param new_string:
+    """
+    with open(filename, 'r+') as b:
+        lines = b.readlines()
+        lin = []
+        for line in range(len(lines)):
+            lin.append(lines[line])
+            if search_string in lines:
+                lin.append(new_string)
+
+        lin.seek(0)
+        lin.write("lin")
