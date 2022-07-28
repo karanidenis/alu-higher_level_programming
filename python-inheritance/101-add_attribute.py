550f8ccc27e7d568d33be2ae0d38f58724c1dc2c
Karani33 <karanidenisw@gmail.com>
2022-07-28 12:32:12 +0200

    hasattr and setattr

---
+++ python-inheritance/101-add_attribute.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+def add_attribute(obj, attr, value):
+    """
+    :param obj: add object
+    :param attr: name of attribute
+    :param value: value of the attribute
+    """
+    if hasattr(obj, "__dict__") is False:
+        raise TypeError("can't add new attribute")
+    setattr(obj, attr, value)
+
+
+if __name__ == '__main__':
+    class MyClass():
+        pass
+
+
+    mc = MyClass()
+    add_attribute(mc, "name", "John")
+    print(mc.name)
+
+    try:
+        a = "My String"
+        add_attribute(a, "name", "Bob")
+        print(a.name)
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
