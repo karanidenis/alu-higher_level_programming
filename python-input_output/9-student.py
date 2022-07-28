668551041125a444fbc0c489f2407a62ab156ace
Karani33 <karanidenisw@gmail.com>
2022-07-29 00:23:04 +0200

    dict repr

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -4,19 +4,16 @@
 
 class Student:
     """ define function"""
+    def __init__(self, first_name, last_name, age):
+        """
+        :param self:
+        :param first_name:
+        :param last_name:
+        :param age:
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-
-def __init__(self, first_name, last_name, age):
-    """
-    :param self:
-    :param first_name:
-    :param last_name:
-    :param age:
-    """
-    self.first_name = first_name
-    self.last_name = last_name
-    self.age = age
-
-
-def to_json(self):
-    return self.__dict__
+    def to_json(self):
+        return self.__dict__
