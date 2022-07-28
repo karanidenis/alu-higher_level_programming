d08aea432cb1843634d01d52f80ff56e2fed6deb
Karani33 <karanidenisw@gmail.com>
2022-07-28 22:54:13 +0200

    function that appends a file and counts the number of characters

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -8,3 +8,18 @@ def write_file(filename="", text=""):
     with open(filename, mode='w', encoding='utf-8') as a_text:
 
         return a_text.write(str(text))
+
+#!/usr/bin/python3
+""" append and counting number of characters in a file"""
+
+
+def write_file(filename="", text=""):
+    """Appends a string to the end of a UTF8 text file.
+        Args:
+            filename (str): The name of the file to append to.
+            text (str): The string to append to the file.
+        Returns:
+            The number of characters appended.
+    """
+    with open(filename, 'a', encoding='utf-8') as b:
+        return b.write(text)
--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-""" append and counting number of characters in a file"""
+"""Defines a file-appending function."""
 
 
-def write_file(filename="", text=""):
+def append_write(filename="", text=""):
     """Appends a string to the end of a UTF8 text file.
-        Args:
-            filename (str): The name of the file to append to.
-            text (str): The string to append to the file.
-        Returns:
-            The number of characters appended.
+    Args:
+        filename (str): The name of the file to append to.
+        text (str): The string to append to the file.
+    Returns:
+        The number of characters appended.
     """
-    with open(filename, 'a', encoding='utf-8') as b:
+    with open(filename, mode="a", encoding="utf-8") as b:
         return b.write(text)
