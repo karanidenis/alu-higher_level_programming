0428bdb509e2e03c6793eec402277831b7e5302e
Karani33 <karanidenisw@gmail.com>
2022-08-03 15:13:54 +0200

    display body and error

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     req = urllib.request.Request(sys.argv[1])
     try:
         with urllib.request.urlopen(req) as qer:
-            print(qer.decode('utf-8'))
+            print(qer.read().decode('utf-8'))
 
     except urllib.error.HTTPError as e:
         print("Error code: ", e.code)
