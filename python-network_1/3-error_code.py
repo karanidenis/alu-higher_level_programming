fe64ce55c317bf61f7f4f4036bdba7a49ccdfaa5
Karani33 <karanidenisw@gmail.com>
2022-08-03 15:12:23 +0200

    display body and error

---
+++ python-network_1/3-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Python script that takes in a URL,
+sends a request to the URL and displays the body of the response
+(decoded in utf-8).
+"""
+import urllib.request
+import urllib.error
+import sys
+
+
+if __name__ == '__main__':
+    req = urllib.request.Request(sys.argv[1])
+    try:
+        with urllib.request.urlopen(req) as qer:
+            print(qer.decode('utf-8'))
+
+    except urllib.error.HTTPError as e:
+        print("Error code: ", e.code)
