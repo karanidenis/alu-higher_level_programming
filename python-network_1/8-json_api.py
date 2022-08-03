a27ad902e7acc88c67069e895954e13c849e4a35
Karani33 <karanidenisw@gmail.com>
2022-08-03 21:16:31 +0200

    display Json id and name

---
+++ python-network_1/8-json_api.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Python script that takes in a letter
+and sends a POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter."""
+
+
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    data = {"q": ""}
+
+    try:
+        data['q'] = [sys.argv[1]]
+    except:
+        pass
+    req = requests.POST('http://0.0.0.0:5000/search_user', data)
+
+    try:
+        json_1 = req.json()
+        if not json_1:
+            print("No result")
+        else:
+            print("{} {}".format(json_1.get('id'), json_1.get('name')))
+    except:
+        print("Not a valid JSON")
