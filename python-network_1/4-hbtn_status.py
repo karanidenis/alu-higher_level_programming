e9d0628f6bcd95c5555a409301ba1d60aff52cc5
Karani33 <karanidenisw@gmail.com>
2022-08-03 16:07:30 +0200

    fetch url and display body

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -2,10 +2,11 @@
 """Python script that fetches https://intranet.hbtn.io/status
  using the package requests"""
 
-import urllib.request
+import requests
+
+
 if __name__ == '__main__':
-    req = urllib.request.Request('https://intranet.hbtn.io/status')
-    with urllib.request.urlopen(req) as file:
-        print("Body response:")
-        print('\t- type: {}'.format(type(file)))
-        print('\t- content: {}'.format(file))
+    req = requests.get('https://intranet.hbtn.io/status').text
+    print("Body response:")
+    print('\t- type: {}'.format(type(req)))
+    print('\t- content: {}'.format(req))
