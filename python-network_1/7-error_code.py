32b006fb08b58fe2f8217761c8c38c7909658c14
Karani33 <karanidenisw@gmail.com>
2022-08-03 17:06:32 +0200

    display body

--- python-network_1/7-error_code.py
+++ python-network_1/7-error_code.py
@@ -3,16 +3,15 @@
 sends a request to the URL and displays the body of the response
 (decoded in utf-8).
 """
-import urllib.request
-import urllib.error
+import requests
 import sys
 
 
 if __name__ == '__main__':
-    req = urllib.request.Request(sys.argv[1])
+    req = requests.get(sys.argv[1])
     try:
-        with urllib.request.urlopen(req) as qer:
-            print(qer.read().decode('utf-8'))
+        req.raise_for_status()
+        print(req.text)
 
-    except urllib.error.HTTPError as e:
-        print("Error code:", e.code)
+    except Exception as e:
+        print("Error code:", req.status_code)
