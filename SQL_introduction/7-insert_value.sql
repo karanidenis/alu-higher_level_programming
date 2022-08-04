28035fc68094a0087b47084a076509080e354760
Karani33 <karanidenisw@gmail.com>
2022-08-04 10:21:29 +0200

    insert rows from tables

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,3 +1,2 @@
 -- script that inserts a new row in the table first_table (database hbtn_0c_0)
-INSERT INTO first_table(
-        VALUES (id = 89, name = Best School));
+INSERT INTO first_table (id = 89, name = Best School);
