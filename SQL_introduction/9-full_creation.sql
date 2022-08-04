3260b968b8d400326e506bb289fb5b6584157884
Karani33 <karanidenisw@gmail.com>
2022-08-04 10:38:14 +0200

    create a table

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- CREATE A TABLE WITH MULTIPLE ROWS
-CREATE TABLE second_table AND INSERT INTO second_table
+CREATE TABLE second_table IF NOT EXISTS AND INSERT INTO second_table
 (id) VALUES (1,2,3,4)
 (name) VALUES ("john","Alex","Bob","George")
 (score) VALUES (10,3,14,8)
