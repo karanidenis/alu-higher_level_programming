a082fcc6bfc57ce0ba58a8231dd4cfbffeabc520
Karani33 <karanidenisw@gmail.com>
2022-08-05 01:38:18 +0200

    CREATING TABLES WITH UNIQUE DESCRIPTION

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -3,6 +3,7 @@
 -- state_id INT, can’t be null and must be a FOREIGN KEY that references to id of the states table
 -- name VARCHAR(256) can’t be null
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
-state_id INT NOT NULL FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id),
-name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (id INT UNIQUE AUTO_INCREMENT NOT NULL,
+state_id INT NOT NULL, name VARCHAR(256) NOT NULL,
+PRIMARY KEY,
+FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id));
