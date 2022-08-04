4eecb7fd0e914b4827b2d908d8fdf35a31ec4b7d
Karani33 <karanidenisw@gmail.com>
2022-08-04 09:22:42 +0200

    CREATE tables with data types in

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,5 +1,5 @@
 -- create table with data types
 -- data types are INT and VARCHAR
-CREATE TABLE IF NOT EXISTS first_table(
-ID,
-NAME VARCHAR(256));
+CREATE TABLE IF NOT EXISTS first_table (
+id INT,
+name VARCHAR(256));
