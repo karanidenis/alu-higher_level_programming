9f859f3dc4c3a24c2009d1ecbcfdcafd0b9e3ea1
Karani33 <karanidenisw@gmail.com>
2022-08-05 00:33:35 +0200

    GRANTS and creating a user

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- script that creates the MySQL server user user_0d_1
 CREATE USER user_0d_1 IDENTIFIED BY user_0d_1_pwd IF NOT EXISTS
-GRANT ALL TO user_0d_1;
+GRANT ALL PRIVILEGES ON *.* TO user_0d_1;
