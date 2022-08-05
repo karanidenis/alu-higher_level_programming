ff34d4a4e5e9cd1b30e61406e6c8fd8dd8e584c1
Karani33 <karanidenisw@gmail.com>
2022-08-05 03:49:24 +0200

    genres NOT in Dexter

---
+++ SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,9 @@
+-- script that uses the hbtn_0d_tvshows database to list all genres not linked to the show Dexter
+SELECT tv_genres.name
+FROM tv_genres
+FULL OUTER JOIN tv_show_genres
+    ON tv_genres.id = tv_show_genres.genre_id
+FULL OUTER JOIN tv_shows
+    ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY tv_genres.name;
