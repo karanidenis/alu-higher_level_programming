bf1d294e608b477366b574ba32607b371b84fa89
Karani33 <karanidenisw@gmail.com>
2022-08-05 04:26:17 +0200

    other shows not comedy

--- SQL_more_queries/101-not_a_comedy.sql
+++ SQL_more_queries/101-not_a_comedy.sql
@@ -9,5 +9,5 @@ JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
 WHERE tv_genres.name = "Comedy"
 ORDER BY tv_shows.title)
 comedy_shows ON comedy_shows.title = tv_shows.title
-WHERE tv_shows.title IS NULL
+WHERE tv_shows.title is NULL
 ORDER BY tv_shows.title;
