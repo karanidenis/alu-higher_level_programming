11c9d89650f9f6635913a92ab959ad2581fc9fc2
Karani33 <karanidenisw@gmail.com>
2022-08-05 02:46:23 +0200

    genres

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,7 +1,6 @@
 -- script that lists all shows contained in the database hbtn_0d_tvshows.
 -- If a show doesn’t have a genre, display NULL
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_show_genres
-RIGHT JOIN tv_show_shows
-ON tv_show_genres.show_id = tv_shows.id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
