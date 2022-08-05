a271ae8f70caa69c7abdb50fc3035e2456e3fea0
Karani33 <karanidenisw@gmail.com>
2022-08-05 04:04:04 +0200

    genres NOT in Dexter

--- SQL_more_queries/100-not_my_genres.sql
+++ SQL_more_queries/100-not_my_genres.sql
@@ -7,5 +7,6 @@ WHERE tv_genres.name NOT IN
 (SELECT tv_genres.name FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_shows.title = 'Dexter')
+WHERE tv_shows.title = 'Dexter'
+ORDER BY tv_genres.name)
 ORDER BY tv_genres.name;
