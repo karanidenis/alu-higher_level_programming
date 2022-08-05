eb0f639438110a992a3e896dd4e81b1ac439093e
Karani33 <karanidenisw@gmail.com>
2022-08-05 04:46:02 +0200

    rating

--- SQL_more_queries/103-rating_genres.sql
+++ SQL_more_queries/103-rating_genres.sql
@@ -1,6 +1,6 @@
 -- script that lists all genres in the database hbtn_0d_tvshows_rate by their rating.
 SELECT tv_genres.name, SUM(tv_genres_rating.rate) AS rating
-    FROM tv_genres
-    JOIN tv_genres_rating ON tv_genres_rating.genre_id = tv_genres.id
-    GRANT BY tv_genres
-    ORDER BY rating DESC;
+FROM tv_genres
+JOIN tv_genres_rating ON tv_genres_rating.genre_id = tv_genres.id
+GROUP BY tv_genres
+ORDER BY rating DESC;
