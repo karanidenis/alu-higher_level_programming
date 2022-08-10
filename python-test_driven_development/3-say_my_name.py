78079f41a6d8823b08b8064896b6663a4fb7a1e7
Karani33 <karanidenisw@gmail.com>
2022-08-10 22:58:11 +0200

    say my name  module

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" module to print names """
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    :param first_name:
+    :param last_name: a string
+    :return: print first and last name as strings
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
+
+
+if __name__ == '__main__':
+
+    say_my_name("John", "Smith")
+    say_my_name("Walter", "White")
+    say_my_name("Bob")
+    try:
+        say_my_name(12, "White")
+    except Exception as e:
+        print(e)
