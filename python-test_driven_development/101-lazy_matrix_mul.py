2bd05aafb5551ebf9d828f3be82c9cc76e8e2ebe
Karani33 <karanidenisw@gmail.com>
2022-08-12 13:53:48 +0200

    matrix multiplication

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""mutliply 2 matrices using Numpy module """
+"""multiply 2 matrices using Numpy module """
 
 import numpy as np
 
@@ -11,16 +11,3 @@ def lazy_matrix_mul(m_a, m_b):
     :return: using Numpy do m_a*m_b
     """
     return np.matmul(m_a, m_b)
-
-
-if __name__ == '__main__':
-    square_matrix_simple = __import__('0-square_matrix_simple').square_matrix_simple
-    matrix = [
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9]
-    ]
-
-    new_matrix = square_matrix_simple(matrix)
-    print(new_matrix)
-    print(matrix)
