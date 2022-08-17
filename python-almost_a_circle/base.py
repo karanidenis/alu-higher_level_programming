491a3e1c2613f154323309d7c9b6e4932b7b6c6c
Karani33 <karanidenisw@gmail.com>
2022-08-17 10:54:35 +0200

    display id

---
+++ python-almost_a_circle/base.py
@@ -0,0 +1,31 @@
+#!/usr/pyhton3
+""" module class with private class attribute __nb_objects = 0
+class constructor: def __init__(self, id=None):
+"""
+
+
+class Base:
+    """ class base with private attributes and constructors """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """initialise instance
+        :param id:
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+
+if __name__ == "__main__":
+
+    b1 = Base()
+    print(b1.id)
+
+    b2 = Base()
+    print(b2.id)
+
+    b3 = Base()
+    print(b3.id)
