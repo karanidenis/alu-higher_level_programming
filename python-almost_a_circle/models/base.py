94cfb280b7a727e8b000428ccd6a0fa08aee1e6a
Karani33 <karanidenisw@gmail.com>
2022-08-18 12:59:54 +0200

    display id

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -29,3 +29,44 @@ if __name__ == "__main__":
 
     b3 = Base()
     print(b3.id)
+
+    # print(Base(12).__nb_instances) #private instance
+
+    b1 = Base(None)  # None id
+    b2 = Base(12)
+    b3 = Base(None)
+    print(b1.id, b3.id - 1)
+
+    b = Base(12)  # unique id
+    b.id = 15
+    print(b.id)
+
+    b = Base("hello")  # string id
+    print(b.id)
+
+    b = Base(2.2)  # float id
+    print(b.id)
+
+    b = Base(complex(5))  # complex id
+    print(b.id)
+
+    b = Base({"a": 1, "b": 2})  # dict id
+    print(b.id)
+
+    b = Base([1, 2, 3])  # list id
+    print(b.id)
+
+    b = Base((1, 2, 3))  # tuple id
+    print(b.id)
+
+    b = Base({1, 2, 3})  # set id
+    print(b.id)
+
+    b = Base(range(5))  # range id
+    print(b.id)
+
+    b = Base(b'python')  # bytes id
+    print(b.id)
+
+    b = Base(float('nan'))  # nan id
+    print(b.id)
