5daa7cf58f74d3999657a0cf0ebd3a23499947ef
Karani33 <karanidenisw@gmail.com>
2022-08-26 14:58:32 +0200

    square module

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -6,7 +6,7 @@ from .rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """docstring for """
+    """docstring for ."""
 
     def __init__(self, size, x=0, y=0, id=None):
         """ class constructor"""
