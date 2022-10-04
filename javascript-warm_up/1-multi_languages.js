d28624a3ef2bcd1a67d043aebc5aa0b77d2c4aa1
karanidenis <you@example.com>
2022-10-04 09:49:13 +0300

    output is multiple lines

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,3 +1,4 @@
+#!/usr/bin/node
 function print(first) {
     console.log(first)
   }
@@ -5,4 +6,3 @@ function print(first) {
   print('C is fun')
   print('Python is cool')
   print('Javascript is amazing')
-  
