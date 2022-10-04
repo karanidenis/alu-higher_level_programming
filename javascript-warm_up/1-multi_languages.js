78165e310473dd64f6b650f87c2f781d71b31a0a
karanidenis <you@example.com>
2022-10-04 11:22:24 +0300

    output is multiple lines

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const first = 'C is fun';
-const second = 'Python is cool';
-const third = 'Javascript is amazing';
-console.log(first); 
-console.log(second);
-console.log(third);   
+const myFirst = 'C is fun';
+const mySecond = 'Python is cool';
+const myThird = 'Javascript is amazing';
+console.log(myFirst); 
+console.log(mySecond);
+console.log(myThird);   
