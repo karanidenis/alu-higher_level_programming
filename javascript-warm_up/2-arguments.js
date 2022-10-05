7eb5bf160d4fde8223db957392243f5a6b7703cb
karanidenis <you@example.com>
2022-10-05 18:26:53 +0300

    arguments

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const args = process.argv;
-if (args.length === 1) {
+if (args.length === 3) {
   console.log('Argument found');
-} else if (args.length > 1) {
+} else if (args.length > 3) {
   console.log('Arguments found');
 } else {
   console.log('No argument');
