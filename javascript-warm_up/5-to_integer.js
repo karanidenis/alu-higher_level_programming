f9c43a5cef468dd76444ace30a576ad19be22b30
karanidenis <you@example.com>
2022-10-08 11:09:35 +0300

    test if arg is a number

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const args = process.argv;
-if (typeof args[0] != Number) {
+if (typeof args[0] !== Number) {
     console.log("Not a number");
 } else {
     console.log("My number: ", args[0]);
