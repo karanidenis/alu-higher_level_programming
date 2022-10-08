e103e46a766866954f38d16322d183b94403baa0
karanidenis <you@example.com>
2022-10-08 13:02:59 +0300

    print times number of in first argument

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
-const txt = 'C is fun'
+const txt = 'C is fun';
 const args = process.argv;
 if (isNaN(args[2])) {
   console.log('Missing number of occurrences');
 } else {
-  for (txt;;)
-  console.log('C is fun'* args[2]);
+  for (;txt;) {
+    console.log('C is fun' * args[2]);
+  }
 }
