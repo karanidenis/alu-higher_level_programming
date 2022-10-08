1e76da9bccb581fd4b94423b45bf379953781725
karanidenis <you@example.com>
2022-10-08 12:25:36 +0300

    print using loop

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const txt = ['C is fun', 'Python is cool', 'JavaScript is amazing']; 
+const txt = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0;txt[i];) {
   console.log(txt[i]);
   i++;
