c514b8b0572146cef190a11942a1e9ba94b2b695
karanidenis <you@example.com>
2022-10-10 19:31:13 +0300

    Class Rectangle with attributes h and w

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (w == 0 || h == 0) return null;
+    if (w == null || h == null) return null;
     if (w <= 0 || h <= 0) return null;
     this.width = w;
     this.height = h;
