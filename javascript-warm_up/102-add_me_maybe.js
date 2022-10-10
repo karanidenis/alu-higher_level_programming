c459b419c9c1776921af1bf3e8fdd79343c670c1
karanidenis <you@example.com>
2022-10-10 12:40:26 +0300

    make a function visible

--- javascript-warm_up/102-add_me_maybe.js
+++ javascript-warm_up/102-add_me_maybe.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 exports.addMeMaybe = function (number, theFunction) {
-    number = number + 1;
-    theFunction(number)
+  number = number + 1;
+  theFunction(number);
 }
 
-// const ddMeMaybe = require('./102-add_me_maybe').addMeMaybe;
+// const addMeMaybe = require('./102-add_me_maybe').addMeMaybe;
 // addMeMaybe(4, function (nb) {
 //   console.log('New value: ' + nb);
 // });
