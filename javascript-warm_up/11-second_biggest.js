fa8039c02e31a1e745e902c505069c2c4c072dc5
karanidenis <you@example.com>
2022-10-10 11:45:17 +0300

    second biggest of the arguments

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const argsArray = process.argv.slice(2);
 if (argsArray.length <= 1) {
-  console.log('0')
+  console.log('0');
 } else {
   argsArray.sort((a, b) => (a - b));
   argsArray.pop();
