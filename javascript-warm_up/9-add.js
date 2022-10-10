6758d5886eb67c6dd2f44a1a95a0004416b0389e
karanidenis <you@example.com>
2022-10-10 10:08:01 +0300

    add values of two arguments using a function

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 const args = process.argv;
 function add(a, b) {
-    a = Number(args[2]);
-    b = Number(args[3]);
     let sum = a + b;
     console.log(sum);
 }
+add(Number(args[2]), Number(args[3]));
