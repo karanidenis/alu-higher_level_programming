f73f25a1e2b7afc4c08c966164f821afb817013f
karanidenis <you@example.com>
2022-10-10 19:47:00 +0300

    Class Rectangle with attributes h and w

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,17 +1,18 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
-    constructor (w, h) {
-      if (w == null || h == null) return null;
-      if (w <= 0 || h <= 0) return null;
-      this.width = w;
-      this.height = h;
+  constructor (w, h) {
+    if (w == null || h == null) return null;
+    if (w <= 0 || h <= 0) return null;
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
     }
-    print() {
-        for (let i = 0; i < this.height; i++) {
-            console.log('X'.repeat(this.width))
-        }
-    }
-  };
+  }
+};
   
 //   const Rectangle = require('./3-rectangle');
 
