d7b245636bf889bcbe468c3012584172902fb529
karanidenis <you@example.com>
2022-10-11 23:38:59 +0300

    print characters of a movie

--- javascript-web_scraping/100-starwars_characters.js
+++ javascript-web_scraping/100-starwars_characters.js
@@ -9,11 +9,11 @@ request(url, function (error, response, body) {
     const data = JSON.parse(body).characters;
     data.forEach(charUrl => {
       request(charUrl, (err, resp, bdy) => {
-          if (err) console.log(err);
-            else {
-              console.log(JSON.parse(bdy).name);
-            }
-        });
+        if (err) console.log(err);
+        else {
+          console.log(JSON.parse(bdy).name);
+        }
+      });
     });
   }
 });
