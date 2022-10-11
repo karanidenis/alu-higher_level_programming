3566d2b77df99cdd8e16ab3fd81f42af288fe73b
karanidenis <you@example.com>
2022-10-11 14:01:27 +0300

    getting a URL status code

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -2,10 +2,10 @@
 const url = process.argv[2];
 const request = require('request');
 
-request (url, (err, response, bosy)=> {
+request(url, (err, response, bosy) => {
   if (err) {
     console.log(err);
   } else {
-    console.log(`code: ${response.statusCode}`)
+    console.log(`code: ${response.statusCode}`);
   }
 });
