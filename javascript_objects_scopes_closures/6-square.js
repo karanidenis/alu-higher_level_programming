25d422a28398c6e385eb5a29c3cfbef2552487b3
karanidenis <you@example.com>
2022-10-11 09:29:10 +0300

    exporting using extends

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -5,7 +5,7 @@ module.exports = class Square extends square {
 
   charPrint (C) {
     if (C === undefined) {
-    super.Print();
+    super.print();
   } else {
       for (let i = 0; i < this.height; i++) {
         console.log(C.repeat(this.width));
