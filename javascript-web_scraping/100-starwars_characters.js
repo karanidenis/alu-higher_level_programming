8a2b8299e9b198390426c4db20e31c20fd96a80e
karanidenis <you@example.com>
2022-10-11 23:35:06 +0300

    print characters of a movie

--- javascript-web_scraping/100-starwars_characters.js
+++ javascript-web_scraping/100-starwars_characters.js
@@ -1,13 +1,14 @@
 #!/usr/bin/node
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/${process.argv[2]}';
+
 request(url, function (error, response, body) {
-  if (!error) {
+  if (error) {
     console.log(error);
   } else {
     const data = JSON.parse(body).characters;
-      characters.forEach(charUrl => {
-        request(charUrl, (err, response, bdy) => {
+    data.forEach(charUrl => {
+      request(charUrl, (err, resp, bdy) => {
           if (err) console.log(err);
             else {
               console.log(JSON.parse(bdy).name);
