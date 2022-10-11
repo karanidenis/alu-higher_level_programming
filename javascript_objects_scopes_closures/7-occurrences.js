5d18d78b6ae15dfdbf0638acf6488a94f6576057
karanidenis <you@example.com>
2022-10-11 09:43:01 +0300

    number of occurences

--- javascript_objects_scopes_closures/7-occurrences.js
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 
 exports.nbOccurences = function (list, searchElement) {
-    let nb = 0;
-    for (let i = 0; i < list.length; i++){
-        if (searchElement === list[i]) {
-            nb++;
-        }
+  let nb = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (searchElement === list[i]) {
+      nb++;
     }
-    return nb
+  }
+  return nb;
 };
 
 // const nbOccurences = require('./7-occurrences').nbOccurences;
