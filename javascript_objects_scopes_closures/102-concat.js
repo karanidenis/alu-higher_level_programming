c67140d099756db5611eed7a9aebfe3b23b8d3cf
karanidenis <you@example.com>
2022-10-14 08:58:26 +0300

    caoncatenate files

--- javascript_objects_scopes_closures/102-concat.js
+++ javascript_objects_scopes_closures/102-concat.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const fileA = fs.readFileSync(args[0]);
 const fileB = fs.readFileSync(args[1]);
 const fileC = `${fileA}${fileB}`;
-fs.writeFileSync(args[2], fileC, (err) => {
+fs.writeFileSync(args[4], fileC, (err) => {
   if (err) {
     console.log(err);
   }
